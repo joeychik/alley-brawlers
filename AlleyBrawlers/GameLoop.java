ac900e58cfc05e673021bead5af5444978adc8af
Taintacle <30621588+Taintacle@users.noreply.github.com>
2018-05-22 15:13:50 -0400

    redo

--- AlleyBrawlers/GameLoop.java
+++ AlleyBrawlers/GameLoop.java
@@ -9,24 +9,6 @@ import java.util.Random;
 import java.awt.image.*; 
 import javax.imageio.*; 
 
-/*An Example demonstrating a simple game loop
-* 
-* This version includes time.
-* The old version moves the object across the screen based on frame rate (calls to repaint)
-* This version will move the object based on elapsed time to make it consistent regardless of the framRate
-* We will make a new 'Clock' class to track time
-* 
-* @Author Mangat
-*/
-
-//This class is used to start the program and manage the windows
-class GameLoop { 
-
-  public static void main(String[] args) { 
-    GameWindow game= new GameWindow();  
-  }
-
-}
 
 //This class represents the game window
 class GameWindow extends JFrame { 
