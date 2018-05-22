0b7bb0a65f3187c628b979fe3efcd5f3f6e20325
Taintacle <30621588+Taintacle@users.noreply.github.com>
2018-05-22 14:35:59 -0400

    Add files via upload
    CREATed main class

---
+++ AlleyBrawlers/AlleyBrawlersMain.java
@@ -0,0 +1,14 @@
+/*
+ * AlleyBrawlers.java
+ * Alley Brawlers, a 2D two player game where players control characters to fight each other
+ * Authors: Eric Ke, Joey Chik
+ */
+
+
+class Main {
+  
+  public static void main(String[] args){
+    //yes
+    
+  }
+}
