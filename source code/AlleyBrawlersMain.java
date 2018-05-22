26f4ad628f56262a7cbdaec0a6d2ae1f83efa8da
joeychik <joey.chik0103@gmail.com>
2018-05-22 15:17:24 -0400

    reorganized

---
+++ source code/AlleyBrawlersMain.java
@@ -0,0 +1,13 @@
+/*
+ * AlleyBrawlers.java
+ * Alley Brawlers, a 2D two player game where players control characters to fight each other
+ * Authors: Eric Ke, Joey Chik
+ */
+
+
+class Main {
+  
+  public static void main(String[] args) { 
+    GameWindow game= new GameWindow();  
+  }
+}
