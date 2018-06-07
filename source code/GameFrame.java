77febbcd2f6c3e40ee9bae17ef2d90ac3edce1e4
Taintacle <30621588+Taintacle@users.noreply.github.com>
2018-06-07 15:20:30 -0400

    Add files via upload

--- source code/GameFrame.java
+++ source code/GameFrame.java
@@ -7,12 +7,21 @@
 //Graphics &GUI imports
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JLabel;
 import java.awt.Toolkit;
 import java.awt.Graphics;
 import java.awt.Color;
 import java.awt.Image;
 import javax.swing.ImageIcon;  
 import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import javax.swing.BorderFactory;
+import javax.swing.border.EmptyBorder;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 
 import java.awt.Rectangle;
 
@@ -24,31 +33,39 @@ import java.awt.event.KeyListener;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseEvent;
 
-class GameFrame extends JFrame { 
+class GameFrameTwo extends JFrame { 
 
   //class variable (non-static)
    static double x, y;
    static GameAreaPanel gamePanel;
+   
    Floor ground;
    Character player, player2;
-  
-  
+   
   //Constructor - this runs first
-  GameFrame() { 
+  GameFrameTwo() { 
     
-    super("Alley Brawlers");  
+    super("My Game");  
     // Set the frame to full screen 
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.player = new Character(0,579,200,100, 'r');
+    this.player2 = new Character(1600, 579,200,100, 'l');
+    
+    gamePanel = new GameAreaPanel();
+    gamePanel.setBackground(new Color(0, 0, 0, 0));
+    
    // this.setLocationRelativeTo(null); //start the frame in the center of the screen
     this.setSize(Toolkit.getDefaultToolkit().getScreenSize().width,Toolkit.getDefaultToolkit().getScreenSize().height);
     this.setUndecorated(true);  //Set to true to remove title bar
     this.setBackground(new Color(0, 0, 0, 0));
     
+    
+    
+    
     //Set up the game panel (where we put our graphics)
-    gamePanel = new GameAreaPanel();
-    gamePanel.setBackground(new Color(0, 0, 0, 0));
     
     this.add(new GameAreaPanel());
+
     
     MyKeyListener keyListener = new MyKeyListener();
     this.addKeyListener(keyListener);
@@ -57,8 +74,6 @@ class GameFrame extends JFrame {
     this.addMouseListener(mouseListener);
 
     this.requestFocusInWindow(); //make sure the frame has focus   
-    this.player = new Character(0,579,200,100, 'r', "resources/characters/lisa.png");
-    this.player2 = new Character(1600, 579,200,100, 'l', "resources/characters/pikachu.png");
     
     this.setVisible(true);
   
@@ -81,6 +96,7 @@ class GameFrame extends JFrame {
   private class GameAreaPanel extends JPanel {
     Clock clock;
     
+    
     GameAreaPanel() {
       Image pic = new ImageIcon("resources/background.png").getImage();
       //notice the x,y variables that we control from our animate method      
@@ -88,7 +104,9 @@ class GameFrame extends JFrame {
       clock = new Clock(); 
     }
     
-    public void paintComponent(Graphics g) { 
+    public void paintComponent(Graphics g) {
+      
+      Font font = new Font("Arial", Font.PLAIN, 48);
       super.paintComponent(g);
       setDoubleBuffered(true);
       clock.update();
@@ -106,6 +124,11 @@ class GameFrame extends JFrame {
       
       player.draw(g);
       player2.draw(g);
+      g.setFont(font);
+      g.setColor(Color.RED);
+      g.drawString("HP: " + String.valueOf((int)player.getHealth()), 10, 100);
+      g.setColor(Color.RED);
+      g.drawString("HP: " + String.valueOf((int)player2.getHealth()), 1700, 100);
       ground.draw(g);
       repaint();
       
@@ -121,30 +144,36 @@ class GameFrame extends JFrame {
       }
 
       public void keyPressed(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {  //If 'D' is pressed
-          player2.moveRight();
-          player.setDirection('r');
-        } 
-        if (e.getKeyCode() == KeyEvent.VK_LEFT) {  //If 'D' is pressed
-          player2.moveLeft();
-          player.setDirection('l');
-        }
-        if (e.getKeyCode() == KeyEvent.VK_UP) {  //If 'D' is pressed
-          player2.jump();
+        if (!player2.getAttacking()) {
+          if (e.getKeyCode() == KeyEvent.VK_RIGHT) {  //If 'D' is pressed
+            player2.moveRight();
+            player.setDirection('r');
+          } 
+          if (e.getKeyCode() == KeyEvent.VK_LEFT) {  
+            player2.moveLeft();
+            player.setDirection('l');
+          }
+          if (e.getKeyCode() == KeyEvent.VK_UP) {  
+            player2.jump();
+          }
+          if (e.getKeyCode() == KeyEvent.VK_NUMPAD1) {  
+            player2.attack(0, player);
+            System.out.println(player.getHealth());
+          }
         }
         if (!player.getAttacking()) {
           if (KeyEvent.getKeyText(e.getKeyCode()).equals("D")) {  //If 'D' is pressed
             player.moveRight();
             player.setDirection('r');
           } 
-          if (KeyEvent.getKeyText(e.getKeyCode()).equals("A")) {  //If 'D' is pressed
+          if (KeyEvent.getKeyText(e.getKeyCode()).equals("A")) {  
             player.moveLeft();
             player.setDirection('l');
           }
-          if (KeyEvent.getKeyText(e.getKeyCode()).equals("W")) {  //If 'D' is pressed
+          if (KeyEvent.getKeyText(e.getKeyCode()).equals("W")) {  
             player.jump();
           }
-           if (KeyEvent.getKeyText(e.getKeyCode()).equals("F")) {  //If 'D' is pressed
+           if (KeyEvent.getKeyText(e.getKeyCode()).equals("F")) {  
             player.attack(0, player2);
             System.out.println(player2.getHealth());
           }
